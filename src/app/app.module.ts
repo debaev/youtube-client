aaf11e7b20c2f3cb3b55e10e51ffc603800efa4b
MAX <debaev@list.ru>
2022-03-27 21:34:00 +0500

    feat: add all necessary components and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
   ],
   imports: [
     BrowserModule,
