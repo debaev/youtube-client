3d9d1eaf0a76bf43b11c9b6a15dfe33e703dde92
MAX <debaev@list.ru>
2022-04-01 17:44:16 +0500

    feat: add angular-material to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import HeaderComponent from './header/header/header.component';
 import SearchResultsComponent from './search/search-results/search-results.component';
 import SearchItemComponent from './search/search-item/search-item.component';
+import MaterialModule from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import SearchItemComponent from './search/search-item/search-item.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
