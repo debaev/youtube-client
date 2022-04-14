36307c19b732f8dbfcf3f2607db8f854bf697cb4
MAX <debaev@list.ru>
2022-04-14 12:23:27 +0500

    feat: add login page and logic

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +11,8 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
     LoginPageComponent,
   ],
   imports: [
-    CommonModule,
     AuthRoutingModule,
+    SharedModule,
   ],
 })
 export class AuthModule { }
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IUser } from '../../models/userModel';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private loginService: LoginService) {}
 
-  constructor() { }
+  errorMsg = false;
 
-  ngOnInit(): void {
+  user: IUser = {
+    login: '',
+    pass: '',
+  };
+
+  onLogin() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    this.user.login && this.user.pass
+      ? this.loginService.login(this.user)
+      : (this.errorMsg = true);
   }
-
 }
