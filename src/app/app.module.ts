a3a88619fb3d04b75a481e888fcece72d22537fe
MAX <debaev@list.ru>
2022-04-26 13:13:44 +0500

    init: start NgRX task and add all ngrx setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import CoreModule from './core/core.module';
 import { YoutubeInterceptor } from './youtube/interceptors/youtube.interceptor';
+import { reducers, metaReducers } from './redux/reducers';
+import { environment } from '../environments/environment';
+import { AppEffects } from './redux/effects/app.effects';
 
 @NgModule({
   declarations: [
@@ -18,6 +24,11 @@ import { YoutubeInterceptor } from './youtube/interceptors/youtube.interceptor';
     BrowserAnimationsModule,
     CoreModule,
     HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: YoutubeInterceptor, multi: true }],
   bootstrap: [AppComponent],
